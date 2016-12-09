5b299a02ffe0772b66196ac0b90292e9ae437c07
Ubuntu <ubuntu@ip-172-31-49-101.ec2.internal>
2016-12-09 22:34:39 +0000

    Updated script

--- Python_Script/make_hist.py
+++ Python_Script/make_hist.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+
 #Create images
 import glob
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 counter = 0
 
-for name in glob.glob('C:/Users/Jack/Desktop/Data/Graph/*'):
+for name in glob.glob('../dataDir/*'):
+    if "_0" not in name:
+        continue
+    print(name)
+    seq_length = []
     with open(name) as f:
         for line in f:
             data = line.split()
@@ -20,6 +28,6 @@ for name in glob.glob('C:/Users/Jack/Desktop/Data/Graph/*'):
     ax.hist(seq_length, bins=range(min(seq_length), max(seq_length) + 1, 1))
 
     # Save the figure
-    fig.savefig('C:/Users/Jack/Desktop/Data/fig' + str(counter) + '.png')
+    fig.savefig('../figs/' + name.split(".")[2][9:] + '.png')
     counter += 1
     plt.clf()
